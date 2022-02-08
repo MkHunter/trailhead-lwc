ea5419a08bdcdde6931235805051852441dd4613
MkHunter <miguelernesto_23@hotmail.com>
2022-02-08 14:06:20 -0700

    Add bike & app LWC

---
+++ force-app/main/default/lwc/app/app.js
@@ -0,0 +1,20 @@
+// app.js
+import { LightningElement } from 'lwc';
+
+export default class App extends LightningElement {
+    bike = {
+        name : 'Electra X4',
+        description : 'A sweet bike built for comfort.',
+        category : 'Mountain',
+        material : 'Steel',
+        price : '$2,700',
+        pictureUrl : 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg'
+    };
+    ready = false;
+
+    connectedCallback() {
+        setTimeout(() => {
+            this.ready = true;
+        }, 3000);
+    }
+}
